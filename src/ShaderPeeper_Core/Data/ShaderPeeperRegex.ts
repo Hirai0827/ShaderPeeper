239833e47c96b40730ba980785752d8ad2aff025
hirai <ryu1999827@gmail.com>
2021-01-05 21:10:18 +0900

    add: equial variable

--- src/ShaderPeeper_Core/Data/ShaderPeeperRegex.ts
+++ src/ShaderPeeper_Core/Data/ShaderPeeperRegex.ts
@@ -1,7 +1,8 @@
 export class ShaderPeeperRegex{
     static spaceRegex = new RegExp("\\s|\\t|\\n");
     static typeRegex = new RegExp("int|float|vec2|vec3");
+    static equalRegex = new RegExp("(?:\\+=|-=|\\*=|/=|=)")
     static defineRegex = new RegExp(`(${ShaderPeeperRegex.typeRegex.source})(?:${ShaderPeeperRegex.spaceRegex.source}+?)((?:\\w|_)+)(?:${ShaderPeeperRegex.spaceRegex.source}*?)=`);
-    static substitutionRegex = new RegExp(`((?:\\w|_)+)(?:|\\.\\w+)(?:${ShaderPeeperRegex.spaceRegex.source}*?)=`);
+    static substitutionRegex = new RegExp(`((?:\\w|_)+)(?:|\\.\\w+)(?:${ShaderPeeperRegex.spaceRegex.source}*?)${ShaderPeeperRegex.equalRegex.source}`);
     static mainPartRegex = new RegExp(`void(?:${ShaderPeeperRegex.spaceRegex.source}+?)main(?:${ShaderPeeperRegex.spaceRegex.source}*?)\\(.*?\\)(?:${ShaderPeeperRegex.spaceRegex.source}*?){`);
 }
